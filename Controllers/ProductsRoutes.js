a8a767a6323fb922dbfd6b99dc795063e40ab2f5
jamere mccall <jameremccall@gmail.com>
2025-04-23 20:21:05 -0400

    models

--- Controllers/ProductsRoutes.js
+++ Controllers/ProductsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Products = require('../Models/Products.js');
+const Products = require('../models/Products.js');
 
 
 router.get('/', async (req, res) => {
