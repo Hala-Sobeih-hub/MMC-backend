4d7c6f169a21140de638805758680eaab516b2c5
Hala-Sobeih-hub <halasobeih@gmail.com>
2025-04-30 19:24:16 -0400

    booking routes tested and fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -19,11 +19,20 @@ const authMiddleware = async (req, res, next) => {
       token = req.headers.authorization.split(' ')[1]
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
+      console.log(`decoded= ${decoded}`)
+      console.log(`decoded userId= ${decoded.id}`)
+
       // Find the user by ID
-      req.user = await User.findById(decoded.id).select('-password')
-      if (!req.user) {
+      const user = await User.findById(decoded.id).select('-password')
+      console.log(`user= ${user}`)
+
+      if (!user) {
         return res.status(401).json({ message: 'Unauthorized, user not found' })
       }
+
+      // Attach decoded user data to request
+      req.user = user
+      //req.user = { id: user._id } // just attach the userId as 'id'
 
       next()
     } catch (error) {
